a81c994d720a3835f96a510005843f39509816cf
Ярослав Лошкарев <dfdfdf.dfdf.1985@mail.ru>
2023-05-03 13:15:48 +0300

    правки

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, URLField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, Optional, Regexp
 
 
 class URLMapForm(FlaskForm):
@@ -10,6 +10,10 @@ class URLMapForm(FlaskForm):
     )
     custom_id = URLField(
         'Ваш вариант короткой ссылки',
-        validators=[Length(1, 16), Optional()]
+        validators=[
+            Length(1, 16), Optional(),
+            Regexp(regex=r'^[a-zA-Z0-9]+$',
+                   message='Указано недопустимое имя для короткой ссылки')
+        ]
     )
     submit = SubmitField('Создать')
--- yacut/views.py
+++ yacut/views.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-from flask import abort, flash, redirect, render_template
+from flask import flash, redirect, render_template
 
 from . import app, db
 from .forms import URLMapForm
@@ -17,29 +17,25 @@ def get_unique_short_id():
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URLMapForm()
-    if form.validate_on_submit():
-        if form.custom_id.data:
-            short = form.custom_id.data
-        else:
-            short = get_unique_short_id()
-            if short is None:
-                abort(500)
-        if URLMap.query.filter_by(short=short).first():
-            flash(f'Имя {short} уже занято!')
-            return render_template('yacut.html', form=form, urlmap=None)
-        urlmap = URLMap(
-            original=form.original_link.data,
-            short=short
-        )
-        db.session.add(urlmap)
-        db.session.commit()
-        return render_template('yacut.html', form=form, urlmap=urlmap)
-    return render_template('yacut.html', form=form, urlmap=None)
+    if not form.validate_on_submit():
+        return render_template('yacut.html', form=form, urlmap=None)
+    if form.custom_id.data:
+        short = form.custom_id.data
+    else:
+        short = get_unique_short_id()
+    if URLMap.query.filter_by(short=short).first():
+        flash(f'Имя {short} уже занято!')
+        return render_template('yacut.html', form=form, urlmap=None)
+    urlmap = URLMap(
+        original=form.original_link.data,
+        short=short
+    )
+    db.session.add(urlmap)
+    db.session.commit()
+    return render_template('yacut.html', form=form, urlmap=urlmap)
 
 
 @app.route('/<short>')
 def redirect_view(short):
-    urlmap = URLMap.query.filter_by(short=short).first()
-    if urlmap is not None:
-        return redirect(f'{urlmap.original}', code=302)
-    abort(404)
+    urlmap = URLMap.query.filter_by(short=short).first_or_404()
+    return redirect(f'{urlmap.original}', code=302)
